456362d4220a285538641f62278d4c8772c2462e
Danielle Bassetto <danibbassetto@hotmail.com>
2024-06-17 00:15:15 -0300

    fix(menu)

--- ScreenMusic.WebMvc/wwwroot/js/site.js
+++ ScreenMusic.WebMvc/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    getDataTable('#table')
+});
 
-// Write your JavaScript code.
+function getDataTable(id) {
+    $(id).DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Mostrar _START_ at&eacute; _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 at&eacute; 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por página",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Próximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Último"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
+}
